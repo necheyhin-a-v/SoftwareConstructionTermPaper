81e3b27d2f3fb8f21b362911ade4618d3b566362
Aleksandr Necheukhin <necheyhin_a_v@mail.ru>
2016-12-08 23:39:51 +0700

    The event CellMouseEnter for table ModeratorChoice.Info has been deleted because it emits whenever mouse on a cell

--- ViewLayer/ModeratorChoice.Designer.cs
+++ ViewLayer/ModeratorChoice.Designer.cs
@@ -41,21 +41,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxEmployerName = new System.Windows.Forms.TextBox();
             this.tabInfoEmployers = new System.Windows.Forms.TabPage();
+            this.splitContainerInfo = new System.Windows.Forms.SplitContainer();
             this.buttonSearchInfo = new System.Windows.Forms.Button();
+            this.textBoxSearchInfo = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.dataGridInfo = new System.Windows.Forms.DataGridView();
             this.ColumnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnITN = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnPhoneNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.textBoxSearchInfo = new System.Windows.Forms.TextBox();
             this.tabVacancies = new System.Windows.Forms.TabPage();
-            this.buttonSearchVacancy = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.dataGridVacancies = new System.Windows.Forms.DataGridView();
-            this.textBoxSearchVacancy = new System.Windows.Forms.TextBox();
-            this.splitContainerInfo = new System.Windows.Forms.SplitContainer();
             this.splitContainerVacancies = new System.Windows.Forms.SplitContainer();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonSearchVacancy = new System.Windows.Forms.Button();
+            this.textBoxSearchVacancy = new System.Windows.Forms.TextBox();
+            this.dataGridVacancies = new System.Windows.Forms.DataGridView();
             this.ColumnVacancyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnSpecialty = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnEmployer = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -66,17 +66,17 @@
             this.tabControlEmployers.SuspendLayout();
             this.tabRegEmployers.SuspendLayout();
             this.tabInfoEmployers.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridInfo)).BeginInit();
-            this.tabVacancies.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridVacancies)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerInfo)).BeginInit();
             this.splitContainerInfo.Panel1.SuspendLayout();
             this.splitContainerInfo.Panel2.SuspendLayout();
             this.splitContainerInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridInfo)).BeginInit();
+            this.tabVacancies.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerVacancies)).BeginInit();
             this.splitContainerVacancies.Panel1.SuspendLayout();
             this.splitContainerVacancies.Panel2.SuspendLayout();
             this.splitContainerVacancies.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridVacancies)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControlEmployers
@@ -88,7 +88,7 @@
             this.tabControlEmployers.Location = new System.Drawing.Point(0, 0);
             this.tabControlEmployers.Name = "tabControlEmployers";
             this.tabControlEmployers.SelectedIndex = 0;
-            this.tabControlEmployers.Size = new System.Drawing.Size(814, 498);
+            this.tabControlEmployers.Size = new System.Drawing.Size(758, 498);
             this.tabControlEmployers.TabIndex = 4;
             this.tabControlEmployers.SelectedIndexChanged += new System.EventHandler(this.TabControlEmployersSelectedIndexChanged);
             // 
@@ -201,11 +201,31 @@
             this.tabInfoEmployers.Location = new System.Drawing.Point(4, 22);
             this.tabInfoEmployers.Name = "tabInfoEmployers";
             this.tabInfoEmployers.Padding = new System.Windows.Forms.Padding(3);
-            this.tabInfoEmployers.Size = new System.Drawing.Size(749, 461);
+            this.tabInfoEmployers.Size = new System.Drawing.Size(750, 472);
             this.tabInfoEmployers.TabIndex = 1;
             this.tabInfoEmployers.Text = "Сведения";
             this.tabInfoEmployers.UseVisualStyleBackColor = true;
-            this.tabInfoEmployers.Click += new System.EventHandler(this.tabInfoEmployers_Click);
+            // 
+            // splitContainerInfo
+            // 
+            this.splitContainerInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerInfo.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainerInfo.Location = new System.Drawing.Point(3, 3);
+            this.splitContainerInfo.Name = "splitContainerInfo";
+            this.splitContainerInfo.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainerInfo.Panel1
+            // 
+            this.splitContainerInfo.Panel1.Controls.Add(this.buttonSearchInfo);
+            this.splitContainerInfo.Panel1.Controls.Add(this.textBoxSearchInfo);
+            this.splitContainerInfo.Panel1.Controls.Add(this.label5);
+            // 
+            // splitContainerInfo.Panel2
+            // 
+            this.splitContainerInfo.Panel2.Controls.Add(this.dataGridInfo);
+            this.splitContainerInfo.Size = new System.Drawing.Size(744, 466);
+            this.splitContainerInfo.SplitterDistance = 26;
+            this.splitContainerInfo.TabIndex = 6;
             // 
             // buttonSearchInfo
             // 
@@ -215,6 +235,13 @@
             this.buttonSearchInfo.TabIndex = 3;
             this.buttonSearchInfo.Text = "Найти";
             this.buttonSearchInfo.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSearchInfo
+            // 
+            this.textBoxSearchInfo.Location = new System.Drawing.Point(50, 2);
+            this.textBoxSearchInfo.Name = "textBoxSearchInfo";
+            this.textBoxSearchInfo.Size = new System.Drawing.Size(130, 20);
+            this.textBoxSearchInfo.TabIndex = 0;
             // 
             // label5
             // 
@@ -237,12 +264,11 @@
             this.dataGridInfo.Location = new System.Drawing.Point(0, 0);
             this.dataGridInfo.Name = "dataGridInfo";
             this.dataGridInfo.ReadOnly = true;
-            this.dataGridInfo.Size = new System.Drawing.Size(743, 425);
+            this.dataGridInfo.Size = new System.Drawing.Size(744, 436);
             this.dataGridInfo.TabIndex = 1;
-            this.dataGridInfo.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
-            this.dataGridInfo.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
-            this.dataGridInfo.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
-            this.dataGridInfo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseUp);
+            this.dataGridInfo.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridInfo_CellEndEdit);
+            this.dataGridInfo.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridInfo_RowsAdded);
+            this.dataGridInfo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dataGridInfo_MouseUp);
             // 
             // ColumnName
             // 
@@ -268,89 +294,16 @@
             this.ColumnPhoneNumber.Name = "ColumnPhoneNumber";
             this.ColumnPhoneNumber.ReadOnly = true;
             // 
-            // textBoxSearchInfo
-            // 
-            this.textBoxSearchInfo.Location = new System.Drawing.Point(50, 2);
-            this.textBoxSearchInfo.Name = "textBoxSearchInfo";
-            this.textBoxSearchInfo.Size = new System.Drawing.Size(130, 20);
-            this.textBoxSearchInfo.TabIndex = 0;
-            // 
             // tabVacancies
             // 
             this.tabVacancies.Controls.Add(this.splitContainerVacancies);
             this.tabVacancies.Location = new System.Drawing.Point(4, 22);
             this.tabVacancies.Name = "tabVacancies";
             this.tabVacancies.Padding = new System.Windows.Forms.Padding(3);
-            this.tabVacancies.Size = new System.Drawing.Size(806, 472);
+            this.tabVacancies.Size = new System.Drawing.Size(750, 472);
             this.tabVacancies.TabIndex = 2;
             this.tabVacancies.Text = "Вакансии";
             this.tabVacancies.UseVisualStyleBackColor = true;
-            // 
-            // buttonSearchVacancy
-            // 
-            this.buttonSearchVacancy.Location = new System.Drawing.Point(186, 0);
-            this.buttonSearchVacancy.Name = "buttonSearchVacancy";
-            this.buttonSearchVacancy.Size = new System.Drawing.Size(75, 23);
-            this.buttonSearchVacancy.TabIndex = 7;
-            this.buttonSearchVacancy.Text = "Найти";
-            this.buttonSearchVacancy.UseVisualStyleBackColor = true;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(5, 5);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 13);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Поиск";
-            // 
-            // dataGridVacancies
-            // 
-            this.dataGridVacancies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridVacancies.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ColumnVacancyName,
-            this.ColumnSpecialty,
-            this.ColumnEmployer,
-            this.ColumnEmperience,
-            this.ColumnEmploymentType,
-            this.ColumnSalary,
-            this.ColumnDescription});
-            this.dataGridVacancies.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridVacancies.Location = new System.Drawing.Point(0, 0);
-            this.dataGridVacancies.Name = "dataGridVacancies";
-            this.dataGridVacancies.ReadOnly = true;
-            this.dataGridVacancies.Size = new System.Drawing.Size(800, 437);
-            this.dataGridVacancies.TabIndex = 5;
-            this.dataGridVacancies.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridVacancies_CellContentClick);
-            // 
-            // textBoxSearchVacancy
-            // 
-            this.textBoxSearchVacancy.Location = new System.Drawing.Point(50, 2);
-            this.textBoxSearchVacancy.Name = "textBoxSearchVacancy";
-            this.textBoxSearchVacancy.Size = new System.Drawing.Size(130, 20);
-            this.textBoxSearchVacancy.TabIndex = 4;
-            // 
-            // splitContainerInfo
-            // 
-            this.splitContainerInfo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainerInfo.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainerInfo.Location = new System.Drawing.Point(3, 3);
-            this.splitContainerInfo.Name = "splitContainerInfo";
-            this.splitContainerInfo.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainerInfo.Panel1
-            // 
-            this.splitContainerInfo.Panel1.Controls.Add(this.buttonSearchInfo);
-            this.splitContainerInfo.Panel1.Controls.Add(this.textBoxSearchInfo);
-            this.splitContainerInfo.Panel1.Controls.Add(this.label5);
-            // 
-            // splitContainerInfo.Panel2
-            // 
-            this.splitContainerInfo.Panel2.Controls.Add(this.dataGridInfo);
-            this.splitContainerInfo.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainerInfo.Size = new System.Drawing.Size(743, 455);
-            this.splitContainerInfo.SplitterDistance = 26;
-            this.splitContainerInfo.TabIndex = 6;
             // 
             // splitContainerVacancies
             // 
@@ -369,9 +322,52 @@
             // splitContainerVacancies.Panel2
             // 
             this.splitContainerVacancies.Panel2.Controls.Add(this.dataGridVacancies);
-            this.splitContainerVacancies.Size = new System.Drawing.Size(800, 466);
+            this.splitContainerVacancies.Size = new System.Drawing.Size(744, 466);
             this.splitContainerVacancies.SplitterDistance = 25;
             this.splitContainerVacancies.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 5);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Поиск";
+            // 
+            // buttonSearchVacancy
+            // 
+            this.buttonSearchVacancy.Location = new System.Drawing.Point(186, 0);
+            this.buttonSearchVacancy.Name = "buttonSearchVacancy";
+            this.buttonSearchVacancy.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearchVacancy.TabIndex = 7;
+            this.buttonSearchVacancy.Text = "Найти";
+            this.buttonSearchVacancy.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSearchVacancy
+            // 
+            this.textBoxSearchVacancy.Location = new System.Drawing.Point(50, 2);
+            this.textBoxSearchVacancy.Name = "textBoxSearchVacancy";
+            this.textBoxSearchVacancy.Size = new System.Drawing.Size(130, 20);
+            this.textBoxSearchVacancy.TabIndex = 4;
+            // 
+            // dataGridVacancies
+            // 
+            this.dataGridVacancies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridVacancies.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnVacancyName,
+            this.ColumnSpecialty,
+            this.ColumnEmployer,
+            this.ColumnEmperience,
+            this.ColumnEmploymentType,
+            this.ColumnSalary,
+            this.ColumnDescription});
+            this.dataGridVacancies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridVacancies.Location = new System.Drawing.Point(0, 0);
+            this.dataGridVacancies.Name = "dataGridVacancies";
+            this.dataGridVacancies.ReadOnly = true;
+            this.dataGridVacancies.Size = new System.Drawing.Size(744, 437);
+            this.dataGridVacancies.TabIndex = 5;
             // 
             // ColumnVacancyName
             // 
@@ -419,7 +415,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 498);
+            this.ClientSize = new System.Drawing.Size(758, 498);
             this.Controls.Add(this.tabControlEmployers);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
@@ -429,19 +425,19 @@
             this.tabRegEmployers.ResumeLayout(false);
             this.tabRegEmployers.PerformLayout();
             this.tabInfoEmployers.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridInfo)).EndInit();
-            this.tabVacancies.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridVacancies)).EndInit();
             this.splitContainerInfo.Panel1.ResumeLayout(false);
             this.splitContainerInfo.Panel1.PerformLayout();
             this.splitContainerInfo.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerInfo)).EndInit();
             this.splitContainerInfo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridInfo)).EndInit();
+            this.tabVacancies.ResumeLayout(false);
             this.splitContainerVacancies.Panel1.ResumeLayout(false);
             this.splitContainerVacancies.Panel1.PerformLayout();
             this.splitContainerVacancies.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerVacancies)).EndInit();
             this.splitContainerVacancies.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridVacancies)).EndInit();
             this.ResumeLayout(false);
 
         }
